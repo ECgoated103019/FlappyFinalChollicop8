474a955a87e61e755b97e8b82a89b20a88308f04
ECgoated103019 <745707@student.dusd.net>
2023-12-16 12:53:23 -0800

    Need to finish
    I need to add one more thing and watch last video.

--- FlappyFinalCholicop8/Assets/Script/GameContol.cs
+++ FlappyFinalCholicop8/Assets/Script/GameContol.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class GameContol : MonoBehaviour 
+public class GameContol : MonoBehaviour
 {
     public TextMeshProUGUI text;
     public static GameContol instance;
+    public Text scoreText;
     public GameObject gameOverText;
     public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    private int score = 0;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -25,13 +33,23 @@ public class GameContol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
-
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
     public void BirdDied()
     {
         gameOverText.SetActive(true);
         gameOver = true;
     }
-
 }
